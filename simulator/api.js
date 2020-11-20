f2fad11585a2191b464cce0e57e539b1f1dc495c
CarlOsterberg <carl.vilhelms.osterberg@gmail.com>
2020-11-20 14:57:54 +0100

    graphql is possible with express

--- simulator/api.js
+++ simulator/api.js
@@ -1,18 +1,47 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+const s = require("./sim");
+
+let simulator = new s();
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    price: Float
+    wph: Float
+    wpd: Float
+    demand: Float
+    production: Float
+    update_day: Boolean
+    update_hour: Boolean
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello world!';
+    price: () => {
+        return simulator.priceObj.currentPrice;
+    },
+    wph: () => {
+        return simulator.windObj.avgWindHour;
+    },
+    wpd: () => {
+        return simulator.windObj.avgWindDay;
+    },
+    demand: () => {
+        return simulator.consumptionObj.consumption;
+    },
+    production: () => {
+        return simulator.productionObj.prod;
+    },
+    update_hour: () => {
+        simulator.updateHour();
+        return true;
+    },
+    update_day: () => {
+        simulator.updateDay();
+        return true;
     },
 };
 
