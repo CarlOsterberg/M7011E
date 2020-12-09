12143573e5966a93d7c9c228a57e4e17f5d49c5a
Elsa <elsa-jonsson@live.se>
2020-12-09 16:37:31 +0100

    Database started

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ var ssn = "Not set";
 const url = 'mongodb://127.0.0.1:27017'
 const dbName = 'M7011E'
 
+app.get('/', (req, res) => {
+    return res.redirect('/home');
+});
+
 app.get('/home', (req, res) => {
     if (ssn === "Not set") {
         res.render('home', {ssn: "Login"});
@@ -59,23 +63,41 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/logged_in', (req, res) => {
-    return res.end("Already logged in, logout before logging in again");
+    res.render('logged_in', {});
 });
 app.get('/logout', (req, res) => {
     req.session.destroy();
     ssn = "Not set";
     return res.redirect('/home');
 });
+app.get('/createUser', (req, res) => {
+    res.render('createUser', {});
+});
+/**
+app.post('/createUser',function(req,res) {
+    if (ssn === "Not set") {
+        MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+            if (err) return console.log(err)
+            let db = client.db(dbName)
+            let query = {name: req.body.createUser}
 
+    }else{
+        return res.redirect('/logged_in');
+    }
+});
+*/
 app.post('/login',function(req,res) {
     if (ssn === "Not set") {
         MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
             if (err) return console.log(err)
             let db = client.db(dbName)
-            let query = {name: req.body.login}
+            let query = {username: req.body.login}
             db.collection("users").find(query).toArray(function (err, result) {
                 if (err) return console.log(err)
                 client.close();
+                if (result.length<1) {
+                    return res.end("User not found.");
+                }
                 bcrypt.compare(req.body.password, result[0].password).then(function (result) {
                     if (result) {
                         ssn = req.session;
