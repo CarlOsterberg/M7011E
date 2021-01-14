e2a91392dd8fba80ae0915a94e3de1c8085b7c30
elsajonsson <36952699+elsajonsson@users.noreply.github.com>
2021-01-14 19:42:04 +0100

    images added

--- test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = 'T4C0T4C0';
-
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-    console.log(hash)
-});
-
